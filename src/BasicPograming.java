c39f357f83ebd7fb04f8ca178cb0de0c5d0bbe50
Aditya Bahakar <adityabahakar99@gmail.com>
2022-03-15 00:30:59 +0530

    Print the prime factors on N

--- src/BasicPograming.java
+++ src/BasicPograming.java
@@ -3,22 +3,19 @@ import java.util.Scanner;
 public class BasicPograming {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of n");
-        int n = sc.nextInt();
-        System.out.println(Harmonic(n));
-    }
-    public static double Harmonic(int n) {
-        double sum = 0;
-        if (n > 0) {
-            for (double i = 1; i <= n; i++) {
-                sum += (1 / i);
-            }
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        System.out.println("Prime factors for the given numbers:");
+        for(int i=2;i<=n;i++)
+        {
+            while(n%i==0)
+            {
+                n=n/i;
+                System.out.println(i+"");
+            }
         }
-        else {
-            System.out.println("Enter a positive number:");
-        }
-        return sum;
+
     }
 }
