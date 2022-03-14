b664264a7e7a6788f0df255d7f656da49a262282
Aditya Bahakar <adityabahakar99@gmail.com>
2022-03-15 00:54:18 +0530

    swap to no

--- src/BasicPograming.java
+++ src/BasicPograming.java
@@ -2,15 +2,22 @@ import java.util.Scanner;
 
 public class BasicPograming {
 
-    public static void main (String[]args){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter 1st number:");
-            int num1 = sc.nextInt();
-            System.out.println("Enter 2nd number:");
-            int num2 = sc.nextInt();
-            int quotient = (num1 / num2);
-            int remainder = (num1 % num2);
-            System.out.println("Quotient  : " + quotient);
-            System.out.println("Remainder : " + remainder);
-        }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println("Numbers before swaping");
+        System.out.println(num1);
+        System.out.println(num2);
+
+
+        int swap1 = num2;
+        int swap2 = num1;
+
+        System.out.println("Numbers after swaping");
+        System.out.println(swap1);
+        System.out.println(swap2);
+
+    }
     }
