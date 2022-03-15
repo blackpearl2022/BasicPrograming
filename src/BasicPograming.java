e1f3c55e484d679a218d1d1d1374312833d6bdf3
Aditya Bahakar <adityabahakar99@gmail.com>
2022-03-15 06:04:47 +0530

    chcking weather no is even or odd

--- src/BasicPograming.java
+++ src/BasicPograming.java
@@ -3,21 +3,15 @@ import java.util.Scanner;
 public class BasicPograming {
 
     public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
+        System.out.println("Enter a Number :");
+        int n = sc.nextInt();
+        if (n%2==0)
+            System.out.println("Number is Even");
+        else
+            System.out.println("Number is Odd");
 
-        System.out.println("Numbers before swaping");
-        System.out.println(num1);
-        System.out.println(num2);
-
-
-        int swap1 = num2;
-        int swap2 = num1;
-
-        System.out.println("Numbers after swaping");
-        System.out.println(swap1);
-        System.out.println(swap2);
 
     }
     }
